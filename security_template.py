6003d591a51434428117f97f46b579879ac55bcf
SKY <sky012877@gmail.com>
2023-07-18 21:53:18 -0700

    security_template.py - Modified :

--- security_template.py
+++ security_template.py
@@ -1,7 +1,7 @@
 # Security file for you credential of your project
 # !!! Do not save the file to the public repository after fill your data !!!
 # !!! Rename the file to "security.py to use it in your project !!!
-# !!! Add the file to your .gitignore !!! 
+# !!! Add the file to your .gitignore !!!
 
 EMAIL = "your@email"
 PASSW = "Your_password"
