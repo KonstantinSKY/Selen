25876ee259580fb578d805c2e35d4051404f844b
SKY <sky012877@gmail.com>
2023-07-18 21:43:54 -0700

    security_template.py - Added :

---
+++ security_template.py
@@ -0,0 +1,6 @@
+# Security file for you credential of your project
+# !!! Do not save the file to the public repository after fill your data !!!
+
+EMAIL = "your@email"
+PASSW = "Your_password"
+COOKIES = {}
