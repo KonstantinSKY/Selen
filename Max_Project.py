4fcb9f67ffb03af344e851d775b13e0180ccf519
Max <max89.mp@gmail.com>
2023-07-25 19:48:12 -0500

    a

--- Max_Project.py
+++ Max_Project.py
@@ -50,24 +50,21 @@ class FirefoxSearch(unittest.TestCase):
                 print("Not OK value is", i)
 
     def test_Basic_HTML_Form_Example(self):
-        print("Second test")
         driver = self.driver
         driver.get("https://testpages.herokuapp.com/")
-        driver.find_element(By.ID, "htmlformtest").click()
+        driver.find_element(By.ID, "htmlformtest").click() #next page HTML Form Example
         check_box_random = randint(0, 2)
-        driver.find_elements(By.NAME, "radioval")[randint(0, 2)].click()
-        driver.find_elements(By.NAME, "checkboxes[]")[check_box_random].click()
+        driver.find_elements(By.NAME, "radioval")[randint(0, 2)].click() # click random radio every time
+
+        driver.find_elements(By.NAME, "checkboxes[]")[check_box_random].click() # click random checkbox every time
         time.sleep(5)
 
-        # select = Select(driver.find_element(By.NAME, 'dropdown'))
-        # select.select_by_index(randint(0, 5))
-        Select(driver.find_element(By.NAME, 'dropdown')).select_by_index(randint(0, 5))
-        # select.select_by_visible_text("Drop Down Item 1")
-        # select.select_by_value(value)
-        Select(driver.find_element(By.NAME, "multipleselect[]")).select_by_index(randint(0, 3))
+        Select(driver.find_element(By.NAME, 'dropdown')).select_by_index(randint(0, 5)) #choosing random dropdown by Select
+        Select(driver.find_element(By.NAME, "multipleselect[]")).select_by_index(randint(0, 3))#Multiple Select Values
 
         driver.find_elements(By.NAME, "submitbutton")[1].click()
         time.sleep(5)
+        #checking if random checkbox have same value with submited form
         submit_form = driver.find_element(By.ID, "_valuecheckboxes0").text
 
         if "cb" + str(check_box_random + 1) == submit_form:
@@ -76,21 +73,42 @@ class FirefoxSearch(unittest.TestCase):
             print("Form Bad")
 
         time.sleep(10)
+    def test_Dynamic_HTML_TABLE_Tag(self):
+        driver = self.driver
+        wait = WebDriverWait(driver, 5)
+        driver.get("https://testpages.herokuapp.com/")
+        driver.find_element(By.ID, "dynamictablestest").click()  # next page Dynamic Table Test Page
+        time.sleep(5)
+        # checking if title is correct
+        try:
+            assert driver.title == "Table HTML Tag - JavaScript Created"
+            print("Title is Correct. Current Title is:", driver.title)
+
+        except AssertionError:
+            print("Title is different. Current Title is:", driver.title)
+
+    # checking if current urs is CORRECT
+        acct_reg_expected_url = "https://testpages.herokuapp.com/styled/tag/dynamic-table.html"
+        acct_reg_actual_url = driver.current_url
+        if acct_reg_expected_url == acct_reg_actual_url:
+            print('"Account registration" page URL is correct:', driver.current_url)
+        else:
+            print('"Account registration" page URL is wrong:', driver.current_url)
+        #     #check if header h1 text is correct
+        # text_website = driver.find_elements(By.XPATH, "//h1[contains(.,'Dynamic HTML TABLE Tag')]").text
+        # text_expected = "Dynamic HTML TABLE Tag"
+        # if text_website == text_expected:
+        #     try:
+        #         assert text_website == text_expected
+        #         print("Paragraph text is correct. Current text is:", text_website)
+        #     except AssertionError:
+        #         print("Paragraph text is different. Current text is:", text_website)
+        driver.find_element(By.TAG_NAME, "p") # checking first paragraph
+        driver.find_element(By.ID, "refreshtable") # refresh table button
+        driver.find_element(By.ID, "caption") # placeholder Caption
+        driver.find_element(By.ID, "tableid") # placeholder Id
+        #driver.find_elements(By.TAG_NAME, "a") как найти все href в теге а
+
 
     def tearDown(self):
         self.driver.quit()
-
-    # send keywordyds
-    # for i in range(1, 100):
-    #     element_par = (by TAG NAME, "p")
-    #     element_par(be.css_selector, "p.nextid")
-    #     if element_par.get_attribute(f'nextid')) == i":
-    #         check...
-    #     findelem(button).click()
-    #     print(f'custom'-
-    #     if element.get_attribute(f'custom-{i}')) == f"value_{i}"
-    #         check
-    # href_elements = self.driver.find_elements(By.TAG_NAME,"a")
-    # for element in href_elements:
-    #     print(element.text)
-    #     print(element.get_attribute("href"))
